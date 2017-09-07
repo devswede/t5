08d36bb1509aaeb2fd7e5cff6e5728bcf70810f3
Reine Olofsson <reine.olofsson@svenskaspel.se>
2017-09-07 16:17:34 +0200

    abc

--- socket/index.js
+++ socket/index.js
@@ -16,23 +16,13 @@ function Room() {
 
 module.exports = function(io) {
 
-  let colorIndex = 0;
-
-/*
-  setInterval(() => {
-    if (colorIndex > 2) colorIndex = 0;
-    io.emit('stoplight', colors[colorIndex]);
-    console.log('EMIT:' + colors[colorIndex]);
-    colorIndex++;
-  }, 2000);
-*/
   return (client) => {
 
-    console.log('Client connected');
+    console.log('Client connected: ' + client.id);
 
-    client.on('event', function(data){});
-
-    client.on('disconnect', function(){});
+    client.on('disconnect', () => {
+      console.log('Client disconnected: ' + client.id);
+    });
 
     client.on('join', (room) => {
 
@@ -41,49 +31,43 @@ module.exports = function(io) {
         rooms[room] = new Room();
       }
       client.join(room);
+      console.log(client.id + ' joined ' + room);
 
       //Send current state to newly connected client
       client.emit('state', rooms[room]);
       if (configs[room]) {
-        console.log('Emit state for ' + room);
         client.emit('config', configs[room]);
       } else {
-        console.log('Emit default state for ' + room);
         client.emit('config', configs['default']);
       }
 
-      console.log('Client joined ' + room);
     });
 
     client.on('leave', (room) => {
       client.leave(room);
-      console.log('Client left ' + room);
+      console.log(client.id + ' left ' + room);
     });
 
     client.on('stoplight', function(color){
       for (let room in client.rooms) {
-
         if (!rooms[room]) {
           rooms[room] = new Room();
         }
         rooms[room].color = color;
-
         io.to(room).emit('stoplight', color);
+        console.log(client.id + ' sets color ' + color + ' for room ' + room);
       }
-      console.log('Color: ' + color);
     });
 
     client.on('iframe', function(url){
       for (let room in client.rooms) {
-
         if (!rooms[room]) {
           rooms[room] = new Room();
         }
         rooms[room].url = url;
-
         io.to(room).emit('iframe', url);
+        console.log(client.id + ' sets iframe ' + url + ' for room ' + room);
       }
-      console.log('IFrame: ' + url);
     });
 
   };
