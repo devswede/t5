f1f2d81e0fcbcb6a4b02e08c3ffd3a9fd108f737
Reine Olofsson <reine.olofsson@svenskaspel.se>
2017-09-07 21:29:39 +0200

    FU react

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,6 @@ const webpack = require('webpack');
 var nodeExternals = require('webpack-node-externals');
 
 module.exports = [
-    /*
-    {
-        entry: './views/index.js',
-        output: {
-            path: __dirname + '/bin',
-            filename: 'app.bundle.js',
-        },
-        module: {
-            loaders: [{
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-1']
-                }
-            }]
-        }
-
-    }
-  ,*/
   {
     entry: './views/display/index.js',
     output: {
