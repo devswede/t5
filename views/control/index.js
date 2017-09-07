37d819a0651d96a1d3cf1c935c93506595b3a473
Reine Olofsson <reine.olofsson@svenskaspel.se>
2017-09-07 18:16:58 +0200

    xczxc

--- views/control/index.js
+++ views/control/index.js
@@ -2,6 +2,7 @@ $(function() {
   let socket = io(),
       colorBtns = $('.light-button'),
       dummyBtn = $('.btn-dummy'),
+      stoplight = $('.js-stoplight'),
       urlBtnContainer = $('.url-btn-container'),
       dimmer = $('.dimmer'),
       room = new URLSearchParams(window.location.search).get('room'),
@@ -37,6 +38,12 @@ $(function() {
 
     urlBtnContainer.empty();
 
+    if (config.stoplight) {
+      stoplight.removeClass('hide');
+    } else {
+      stoplight.addClass('hide');
+    }
+    
     for(i = 0; i < config.iframes.length; i++) {
       newBtn = dummyBtn.clone();
       newBtn.attr('data-url', config.iframes[i].url);
