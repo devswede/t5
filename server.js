48c6c09f0454bd2bb28e1082c83e429342b36dbb
Reine Olofsson <reine.olofsson@svenskaspel.se>
2017-09-07 10:31:49 +0200

    abc

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-import express from 'express';
-import router from './routes/index';
-
-//var displayFn = require('./routes/display');
-let app = express();
+const express = require('express'),
+		  router = require ('./routes/index'),
+			app = express(),
+			http = require('http').Server(app),
+		  io = require('socket.io')(http);
 
 app.use('/bin', express.static('./bin'));
 app.use('/stylesheets', express.static('./public/stylesheets'));
 
-//app.get('/display', displayFn);
 
 app.use('/', router);
 app.use('/view/*', router);
 
-/*const io = require('socket.io')(app);
 io.on('connection', function(client){
   client.on('event', function(data){});
   client.on('disconnect', function(){});
 });
-*/
 
-app.listen(3000, function () {
+
+http.listen(3000, function () {
 	console.log('Hello World listening on port 3000!');
 });
