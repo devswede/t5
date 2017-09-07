e633639e45559e4e9a3ed0e443fe4f148c700d6c
Reine Olofsson <reine.olofsson@svenskaspel.se>
2017-09-07 10:55:04 +0200

    Socket file

--- socket/index.js
+++ socket/index.js
@@ -1,7 +1,19 @@
 
+const colors = ['red', 'yellow', 'green'];
+
 module.exports = function(io) {
 
   return (client) => {
+
+    let colorIndex = 0;
+
+    setInterval(() => {
+      if (colorIndex > 2) colorIndex = 0;
+      io.emit('stoplight', colors[colorIndex]);
+      console.log('EMIT:' + colors[colorIndex]);
+      colorIndex++;
+    }, 2000);
+
     client.on('event', function(data){});
 
     client.on('disconnect', function(){});
