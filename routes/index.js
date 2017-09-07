2ce7c428bdb1869277223e80e2038b45275bc3c1
Reine Olofsson <reine.olofsson@svenskaspel.se>
2017-09-07 13:37:46 +0200

    aasdas

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,14 @@ router.get('/control', (req, res, next) => {
 
 router.get('/api/wake/:room', (req, res, next) => {
   router.io.to(req.params.room).emit('wake');
-  console.log('Wake emit to ' + req.params.room);
+  console.log(`API - Wake to ${req.params.room}`);
+  res.sendStatus(200, 'OK');
 });
 
 router.get('/api/stoplight/:room/:color', (req, res, next) => {
   router.io.to(req.params.room).emit('stoplight', req.params.color);
-  console.log('Color from API');
+  console.log(`API - Color ${req.params.color} to ${req.params.room}`);
+  res.sendStatus(200, 'OK');
 });
 
 module.exports = router;
