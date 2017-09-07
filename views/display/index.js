680526deeb01165ba124638a60bd8ebfa34e857e
Jimmie Larsson <Jimmie.Larsson@svenskaspel.se>
2017-09-07 11:09:51 +0200

    Stoplight

--- views/display/index.js
+++ views/display/index.js
@@ -1,6 +1,21 @@
-(function() {
-  let socket = io();
-  socket.on('stoplight', function(color){
-    alert(color);
+$(function() {
+  let socket = io(),
+      redLight = $('.red'),
+      yellowLight = $('.yellow'),
+      greenLight = $('.green'),
+      allLights = $('.light');
+  socket.on('stoplight', color => {
+    allLights.removeClass('light-up');
+    switch(color) {
+      case 'red':
+        redLight.addClass('light-up');
+        break;
+      case 'yellow':
+        yellowLight.addClass('light-up');
+        break;
+      case 'green':
+        greenLight.addClass('light-up');
+        break;
+    }
   });
 }());
