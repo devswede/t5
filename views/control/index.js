bb165294db040d92307d1d3245a8f335b2ebffed
Reine Olofsson <reine.olofsson@svenskaspel.se>
2017-09-08 10:46:47 +0200

    dsfsdfsdf

--- views/control/index.js
+++ views/control/index.js
@@ -8,6 +8,7 @@ $(function() {
       urlBtnContainer = $('.url-btn-container'),
       dimmer = $('.dimmer'),
       screen = new URLSearchParams(window.location.search).get('screen'),
+      useKeyboard = (new URLSearchParams(window.location.search).get('keyboard') === '1'),
       msgForm = $('.js-msg-form'),
       screenTimeout,
       screenTimeOutDelay = 45000;
@@ -49,6 +50,61 @@ $(function() {
     setTimeout(onScreenTimeout, screenTimeOutDelay);
   }
 
+  function enableKeyboard() {
+    $('#keyboard').keyboard({
+      language     : 'sv',  // string or array
+      layout       : 'ms-Swedish',
+      position : {
+        of : null,
+        my : 'center bottom',
+        at : 'center bottom',
+        at2: 'center bottom'
+      },
+      reposition : true,
+      usePreview : true,
+      alwaysOpen : false,
+      noFocus : false,
+      userClosed : false,
+      autoAccept : true,
+      ignoreEsc : false,
+      closeByClickEvent : false,
+      css : {
+        input          : 'ui-widget-content ui-corner-all',
+        container      : 'ui-widget-content ui-widget ui-corner-all ui-helper-clearfix',
+        popup: '',
+        buttonDefault  : 'ui-state-default ui-corner-all',
+        buttonHover    : 'ui-state-hover',
+        buttonAction   : 'ui-state-active',
+        buttonDisabled : 'ui-state-disabled',
+        buttonEmpty    : 'ui-keyboard-empty'
+      },
+      appendTo : 'body',
+      resetDefault : false,
+      openOn : 'focus',
+      keyBinding : 'mousedown touchstart',
+      useCombos : true,
+      initialized   : function(e, keyboard, el) {},
+      beforeVisible : function(e, keyboard, el) {},
+      visible       : function(e, keyboard, el) {},
+      beforeInsert  : function(e, keyboard, el, textToAdd) { return textToAdd; },
+      change        : function(e, keyboard, el) {},
+      beforeClose   : function(e, keyboard, el, accepted) {},
+      accepted      : function(e, keyboard, el) {},
+      canceled      : function(e, keyboard, el) {},
+      restricted    : function(e, keyboard, el) {},
+      hidden        : function(e, keyboard, el) {},
+      switchInput : function(keyboard, goToNext, isAccepted) {},
+      create : function(keyboard) { return keyboard.buildKeyboard(); },
+      buildKey : function( keyboard, data ) {
+        return data;
+      },
+      validate : function(keyboard, value, isClosing) {
+        return true;
+      }
+
+    });
+  }
+
   socket.on('config', (config) => {
     let newBtn, i;
 
@@ -62,7 +118,7 @@ $(function() {
 
     if (config.chat) {
       messages.removeClass('hide');
-      $('#keyboard').keyboard();
+      if (useKeyboard) enableKeyboard();
     } else {
       messages.addClass('hide');
     }
